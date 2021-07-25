a4413005dfb20c93e99e20bef19281660f0ccb02
Jwala Mitra <54636176+jwalaarammitra@users.noreply.github.com>
2021-07-25 01:31:41 -0700

    added easy and medium levels

--- run.java
+++ run.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class run {
@@ -18,26 +19,103 @@ public class run {
 
         // read user input
         boolean validInput = false;
+        String level = null;
 
         while (!validInput) {
             Scanner readObj = new Scanner(System.in);
-            String level = readObj.nextLine();
-            System.out.println(level);
-            System.out.println();
+            level = readObj.nextLine();
 
             // if input is not valid
-            if (level.compareTo("E") == 0 && level.compareTo("M") == 0
-                    && level.compareTo("H") == 0 && level.compareTo("e") == 0
-                    && level.compareTo("m") == 0 && level.compareTo("h") == 0) {
+            if (level.compareTo("E") != 0 && level.compareTo("M") != 0
+                    && level.compareTo("H") != 0 && level.compareTo("e") != 0
+                    && level.compareTo("m") != 0 && level.compareTo("h") != 0) {
                 System.out.println("Try again! Your input is invalid.");
                 System.out.println();
             }
             else {
-                System.out.println("valid");
                 validInput = true;
             }
         }
 
+        int count = 0;
+
         // game type by level
+        if (level.compareTo("E") != 0 || level.compareTo("e") != 0) {
+            easy(count);
+        }
+        if (level.compareTo("M") != 0 || level.compareTo("m") != 0) {
+            medium(count);
+        }
+        if (level.compareTo("H") != 0 || level.compareTo("h") != 0) {
+            hard(count);
+        }
     }
+
+    // helper function: manages easy level
+    public static void easy(int count) {
+        for (int i = 0; i < 50; i++) {
+            Random rand = new Random();
+            int upperbound = 15;
+            int a = rand.nextInt(upperbound);
+            int b = rand.nextInt(upperbound);
+            int c = a + b;
+            int answer = 0;
+
+            System.out.println(a + " + " + b + " = ");
+            Scanner readObj = new Scanner(System.in);
+            if (readObj.hasNextInt()) {
+                answer = readObj.nextInt();
+            }
+            else {
+                System.out.println("Try again! Your input is invalid.");
+                System.out.println();
+            }
+
+            if (answer == c) { count++; }
+        }
+    }
+
+    // helper function: manages medium level
+    public static void medium(int count) {
+        for (int i = 0; i < 50; i++) {
+            Random rand = new Random();
+            int upperbound = 15;
+            int a = rand.nextInt(upperbound);
+            int b = rand.nextInt(upperbound);
+            int c;
+
+            boolean add = false;
+
+            if (i % 2 == 0) {
+                add = true;
+                c = a + b;
+                System.out.println(a + " + " + b + " = ");
+            }
+            else {
+                c = a - b;
+                System.out.println(a + " - " + b + " = ");
+            }
+
+            Scanner readObj = new Scanner(System.in);
+            while (!readObj.hasNextInt()) {
+                System.out.println("Try again! Your input is invalid.");
+                if (add) {
+                    System.out.println(a + " + " + b + " = ");
+                }
+                else {
+                    System.out.println(a + " - " + b + " = ");
+                }
+            }
+            int answer = readObj.nextInt();
+
+            if (answer == c) { count++; }
+        }
+    }
+
+    // helper function: manages hard level
+    public static void hard(int count) {
+
+    }
+
+    // CLOSE SCANNERS!
 }
